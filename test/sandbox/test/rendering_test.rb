a5ae1cc69d144b52c5189a36cc4e3ed993b825c3
Mitchell Henke <mitchell.henke@gsa.gov>
2023-10-25 16:51:25 -0500

    add configuration and support for freezing string literals in compiled templates

--- test/sandbox/test/rendering_test.rb
+++ test/sandbox/test/rendering_test.rb
@@ -1094,4 +1094,28 @@ class RenderingTest < ViewComponent::TestCase
 
     assert_selector("script", text: "\n//<![CDATA[\n  \"alert('hello')\"\n\n//]]>\n", visible: :hidden)
   end
+
+  def test_frozen_string_literal_disabled
+    old_value = ViewComponent::Base.config.frozen_string_literal
+    ViewComponent::Base.config.frozen_string_literal = false
+
+    with_new_cache do
+      render_inline(MutatedStringComponent.new)
+      assert_includes rendered_content, "ab"
+    end
+  ensure
+    ViewComponent::Base.config.frozen_string_literal = old_value
+  end
+
+  def test_frozen_string_literal_enabled
+    old_value = ViewComponent::Base.config.frozen_string_literal
+    ViewComponent::Base.config.frozen_string_literal = true
+    with_new_cache do
+      assert_raises FrozenError do
+        render_inline(MutatedStringComponent.new)
+      end
+    end
+  ensure
+    ViewComponent::Base.config.frozen_string_literal = old_value
+  end
 end
